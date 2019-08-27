9cd90c2d9ad30b81f1639c2d502fa3b7bc134a70
Ghassan <g4ss4n@gmail.com>
2019-08-27 23:59:21 +0300

    css stuff

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-var hbs = require('hbs');
-require('handlebars-form-helpers').register(hbs.handlebars);
+var hbs = require("hbs");
+require("handlebars-form-helpers").register(hbs.handlebars);
 
-const app = express();
+var favicon = require("serve-favicon");
+
+var app = express();
+
+app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -23,10 +27,13 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
   res.render("home", {});
 });
+
 app.get("/contact", (req, res) => {
   res.render("contact", {});
 });
+
 app.get("/content", (req, res) => {
   res.render("content", {});
 });
+
 module.exports = app;
--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,20 +1,23 @@
 BEGIN;
 
+    DROP TABLE IF EXISTS contacts
+    CASCADE;
 
-    DROP TABLE IF EXISTS contact CASCADE;
+CREATE TABLE contacts
+(
+    first_name VARCHAR(30),
+    last_name VARCHAR (30),
+    phone_number INTEGER,
+    email VARCHAR(100),
+    interests VARCHAR(200)
+);
 
-    CREATE TABLE contact(
-        first_name VARCHAR(30),
-        last_name VARCHAR (30),
-        phone_number INTEGER,
-        email VARCHAR(100),
-        intersted_about VARCHAR(200));
 
+INSERT INTO contacts
+    (first_name,last_name,phone_number,email,interests)
+VALUES
+    ('Ghassan', 'Gharzuzy', 0544431898, 'ghassan@gmail.com', 'girls, not you & food'),
+    ('Dana', 'Falah', 0540540540, 'dana@gmail.com', 'sport, coding & using the horn 10 times a day'),
+    ('John', 'Doe', 0503584932, 'john@gmail.com', 'life (which I doubt)');
 
-    INSERT INTO contact (first_name,last_name,phone_number,email,intersted_about)
-    VALUES
-    ('Ghassan', 'Gharzuzy', 0544431898,'ghassan@gmail.com','girls'),
-    ('Dana', 'Falah', 0540540540,' dana@gmail.com',' sport'),
-    ('John', 'Doe', 0503584932, 'john@gmail.com', 'life');
-
-    COMMIT;
+COMMIT;
