18ff756a34b9260b1930e6d3330a4bab4ca2c058
Ghassan <g4ss4n@gmail.com>
2019-08-28 13:45:52 +0300

    post data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const postData = require("../src/queries/postData.js");
+
+var bodyParser = require("body-parser");
 var hbs = require("hbs");
-require("handlebars-form-helpers").register(hbs.handlebars);
-
 var favicon = require("serve-favicon");
-
 var app = express();
 
 app.use(favicon(path.join(__dirname, "..", "public", "favicon.ico")));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -34,6 +36,26 @@ app.get("/contact", (req, res) => {
 
 app.get("/content", (req, res) => {
   res.render("content", {});
+});
+
+app.post("/content", urlencodedParser, (req, res) => {
+  // response = {
+  //   first_name: req.body.first_name,
+  //   last_name: req.body.last_name,
+  //   interest: req.body.interests,
+  //   email: req.body.email,
+  //   phone_number: req.body.phone_number
+  // };
+  postData(
+    req.body.first_name,
+    req.body.last_name,
+    req.body.interests,
+    req.body.email,
+    req.body.phone_number,
+    (err, res) => {}
+  );
+  // console.log(response);
+  // res.end(response);
 });
 
 module.exports = app;
