379e88209f4863dfdccd061d50076f14a733a281
DanaEFalah <dana.e.falah@gmail.com>
2019-08-28 15:52:04 +0300

    tests

---
+++ src/tests/routes.test.js
@@ -0,0 +1,15 @@
+const test = require('tape');
+const request = require('supertest');
+const app = require('../app.js');
+
+test('All routes should return the expected results', t => {
+  request(app)
+    .get('/content/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
+
