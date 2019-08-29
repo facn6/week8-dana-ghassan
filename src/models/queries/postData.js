5bbe3043c1b09501c4dc128fea92223186ca687f
DanaEFalah <dana.e.falah@gmail.com>
2019-08-29 14:34:51 +0300

    fix isuues

--- src/models/queries/postData.js
+++ src/models/queries/postData.js
@@ -5,17 +5,21 @@ const postData = (
   last_name,
   interests,
   email,
-  phone_number,
-  cb
-) => {
-  dbConnection.query(
+  phone_number) => {
+  return new Promise((resolve,reject)=>{
+    dbConnection.query(
     "INSERT INTO contacts (first_name,last_name,interests,email,phone_number) VALUES ($1, $2, $3 ,$4,$5)",
     [first_name, last_name, interests, email, phone_number],
     (err, res) => {
-      if (err) return cb(err);
-      cb(null, res);
-    }
-  );
-};
+      if(err)
+      {
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  
+});
+}
 
 module.exports = postData;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js')
 
 
-const port = 5551
+const port = 5555
 app.listen(port, () => {
   console.log(`Our app is running on http://localhost:${port}`);
 });
